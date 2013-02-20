bdce361b387a8033c4a4863621399f1f9eb54210
Thomas Halstead <tom.m.halstead@gmail.com>
2013-02-20 03:25:50 -0800

    Spelling, Reassignments

--- include/geometry.h
+++ include/geometry.h
@@ -10,11 +10,11 @@ double Slope(double x1, double y1, double x2, double y2);
 
 
 double Perpendicular(double slope);
-//Not Assigned
+//Assigned to: Davina Doedijono
 
 
 double Angle(double a, double b, double c); // c is the side opposite the angle to be calculated
-//Assigned to: Davina Boedyono
+//Assigned to: Davina Boedijono
 
 
 void Segment(double x1, double y1, double slope, double distance, double& x2, double& y2);
