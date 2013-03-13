100aaf7e5c2d9c0b8e9e2f56580e31a0b2455326
Thomas Halstead <tom.m.halstead@gmail.com>
2013-03-13 15:04:31 -0700

    Update geometry.h

--- include/geometry.h
+++ include/geometry.h
@@ -3,24 +3,24 @@
 
 #include <math.h>
 
-double distance(double x1, double y1, double x2, double y2)
+double distance(double x1, double y1, double x2, double y2);
 /* Written by: Dylan La Com
 Returns the distance between two points.
 */
 
-double slope(double x1, double y1, double x2, double y2)
+double slope(double x1, double y1, double x2, double y2);
 /* Written by: Shang Yi (Sam) Huang
 Calculates the slope between two points.
 */
 
 
-double perpendicular(double slope)
+double perpendicular(double slope);
 /*
 Written by: Davina Doedijono
 Returns a slope perpendicular to the given slope
 */
 
-double angle(double a, double b, double c)
+double angle(double a, double b, double c);
 /*
 Written by: Davina Boedijono
 Given a triangle with side lengths a,b and c, returns
@@ -28,7 +28,7 @@ the value of the angle opposite side c in degrees.
 */
 
 
-void segment(double x1, double y1, double slope, double distance, double& x2, double& y2)
+void segment(double x1, double y1, double slope, double distance, double& x2, double& y2);
 /*
 Written by: Pui Yee Lee
 Calculates the coordinates the point which forms the line segment at the specified slope
