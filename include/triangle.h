fc2370d99764ad2f6ad9fa0eaf45bc6af375539b
Thomas Halstead <tom.m.halstead@gmail.com>
2013-03-13 14:56:30 -0700

    add private member

--- include/triangle.h
+++ include/triangle.h
@@ -9,6 +9,8 @@ class Triangle: public Polygon {
 public:
     Triangle(const Point& p1, const Point& p2, const Point& p3);
     double Angle(int i); //This will return the angle opposite the side stored at index i
+private:
+    double Triangle::getLength(int point);
 };
 
 #endif // TRIANGLE_H
